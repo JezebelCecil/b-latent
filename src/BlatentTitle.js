69f63f597e61dd701043274d1b655372e6f8cd1e
Matthew Read <mattread90@gmail.com>
2017-08-02 18:23:30 +0100

    Create basic title component

---
+++ src/BlatentTitle.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class BlatentTitle extends Component {
+  render() {
+    return (
+      <div className="title-container">
+        <div className="title-text">
+          <h1>B-LATENT</h1>
+        </div>
+        <div className="subtitle-text">
+          <h3>revisions of the obvious</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BlatentTitle;
