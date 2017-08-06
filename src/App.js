903c693ba53c7111ad4de1d0c7a1d44634eb7025
Matthew Read <mattread90@gmail.com>
2017-08-06 19:15:19 +0100

    Add media queries to resize title text more appropriately on different sized devices

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App-container">
-        <div>
+        <div style={{ width: '100%' }}>
           <BlatentTitle />
         </div>
         <div>
